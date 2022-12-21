48d5023e602e80cfba7299b8f838470c45ab7056
mots-and-code <hacmotshiro2@yahoo.co.jp>
2022-12-22 04:48:29 +0900

    LINEBot 大幅アップデート 新規登録の案内機能

---
+++ app/Common/LINEMessageBuilder.php
@@ -0,0 +1,173 @@
+<?php
+
+namespace App\Common;
+
+use Illuminate\Database\Eloquent\Collection;
+
+use LINE\LINEBot\MessageBuilder;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateMessageBuilder;
+use LINE\LINEBot\MessageBuilder\MultiMessageBuilder;
+use LINE\LINEBot\MessageBuilder\StickerMessageBuilder;
+use LINE\LINEBot\MessageBuilder\ImageMessageBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ButtonTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder;
+use LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder;
+use LINE\LINEBot\TemplateActionBuilder\MessageTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\PostbackTemplateActionBuilder;
+use LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder;
+
+use App\Models\Student;
+use App\Models\LineUser;
+use App\Models\EntexHistory;
+
+class LINEMessageBuilder
+{
+    //MessageBuilderをつくって返すクラスです
+
+    //postback data の識別につかう
+    const PB_INDI_NEWREGISTRATION = "新規登録方法を案内する";
+
+    //新規登録の案内メッセージ
+    public function introduceRegistration($fromNotRegistered = false):MessageBuilder{
+
+        $multiMes = new MultiMessageBuilder();
+
+        if($fromNotRegistered){
+            //0つ目 入退室履歴を見ようとして、まだ未登録だった場合は、先にこのメッセージを出す
+            $multiMes->add(new TextMessageBuilder("初期登録がまだのようです"));
+        }
+        //1つ目 説明画像
+        $originalContentURL = url("images/line_message_newregister.png");
+        $previewImageURL = url("images/line_message_newregister_compressed.png");
+        if(env('APP_DEBUG')){
+            //httpを強制的にhttpsにする
+            $originalContentURL = str_replace("http://","https://",$originalContentURL);
+            $previewImageURL = str_replace("http://","https://",$previewImageURL);
+        }
+        // $multiMes->add(new ImageMessageBuilder(url("images/line_message_newregister.png"),url("images/line_message_newregister.png")));
+        $multiMes->add(new ImageMessageBuilder($originalContentURL,$previewImageURL));
+        //2つ目 説明文
+        $multiMes->add(new TextMessageBuilder("上図のように\n（お子様のお名前)-(識別コード)を送ってください\nお名前はフルネームスペースなし\n識別コードは生年月日の月日4桁\nでお願いします"));
+
+        return $multiMes;
+    }
+
+    //LINEUser登録が上手くいったとき
+    public function registerCompleted():MessageBuilder{
+
+        $multiMes = new MultiMessageBuilder();
+        $multiMes->add(new TextMessageBuilder("正しく登録できました。ありがとうございました。"));
+        $multiMes->add(new StickerMessageBuilder('8515','16581243'));//ありがとうございました。のスタンプ
+
+        return $multiMes;
+    }
+    //登録しようとしたが、名前か識別コードが一致しなかったとき
+    public function notMatch():MessageBuilder{
+        return new TextMessageBuilder("お子様の名前か識別コードが異なるようです。\nお子様の名前に識別コードを添えて送信してください。\n例)山田太郎-0229");
+    }
+    //登録しようとしたが、既に登録されていたとき
+    public function alreadyExists():MessageBuilder{
+        return new TextMessageBuilder("既にご登録頂いています");
+    }
+    //登録処理の途中でキャンセルになった（プロファイル情報の取得ができなかった）
+    public function registerCanceled():MessageBuilder{
+
+        return new TextMessageBuilder("登録処理の途中でエラーが発生しました。\n恐れ入りますが、もう一度お試し頂くかサポーターにお問い合わせ下さい。\n詳細:プロファイル情報の取得に失敗");
+    }
+    
+    //入退室履歴を確認しようとして、初期登録がまだの場合
+    public function notRegistered():MessageBuilder{
+        return $this->introduceRegistration(true); 
+    }
+    //入退室履歴メッセージ $usersはLINEUser型である必要
+    public function entexHistory(Collection $users):MessageBuilder{
+
+        $multiMes = new MultiMessageBuilder();
+
+        foreach($users as $user){
+            //生徒単位のループ
+
+            //入退室履歴テーブルから情報を取得する
+            $litems = EntexHistory::getEntexHistory($user->student_id);
+            $student = Student::find($user->student_id);
+
+            $message = "";
+            if(count($litems)<= 0){
+
+                $multiMes->add(new TextMessageBuilder(($student->messageDispName)."さんはまだ入退室履歴がありません"));
+            
+            }
+            else{
+                /*作成するメッセージのイメージ
+                斉藤 陽翔さんの入退室履歴
+
+                11/29 18:16 入室
+                11/17 06:35 入室
+                11/17 06:32 入室
+                11/16 14:40 入室
+                11/16 14:40 入室
+                11/16 14:40 入室
+                ※直近10件を表示しています
+
+                */
+
+                // $message = $student->messageDispName."さんの入退室履歴";//NG
+                $message = ($student->messageDispName)."さんの入退室履歴\n\n";//( )がないとエラー
+
+                $i = 0;
+                foreach($litems as $item){
+                    if($i>9){
+                        break;
+                    }
+                    $typeName = $item->type=='1'?'入室':'退室';
+                    $message = $message.date('m/d H:i', strtotime(($item->entex_datetime)))." ".$typeName."\n";
+                    
+                    $i++;
+                }
+                $message=$message."※直近10件を表示しています";
+
+                $multiMes->add(new TextMessageBuilder($message));
+            }
+
+        }
+
+        return $multiMes;
+    }
+    //どの条件にも該当しなかった場合のメッセージ
+    // return MessageBuilder
+    public function nonApplicable():MessageBuilder{
+
+        $multi =new MultiMessageBuilder();
+
+        //1通目 テキストメッセージで案内
+        $multi->add(new TextMessageBuilder("本チャットでお答えできる内容ではないようです。\n以下メニューから選んでください"));
+
+        //2通目 カルーセルテンプレートメッセージで、次のアクションを提案する
+        //1列目
+        $imageURL = url("images/line_message_carousel_register.png");
+        if(env('APP_DEBUG')){
+            //httpを強制的にhttpsにする
+            $imageURL = str_replace("http://","https://",$imageURL);
+        }
+        $action = new PostbackTemplateActionBuilder('こちらをタップ',self::PB_INDI_NEWREGISTRATION,null,null,null);
+        $ccs[] = new CarouselColumnTemplateBuilder("新規登録案内","新規登録する方法を見るには下のボタンをタップしてください",$imageURL,[$action],null,null);
+        
+        //2 ~ (N-1)列目
+
+        //最終列
+        $imageURL = url("images/line_message_carousel_inquiry.png");
+        if(env('APP_DEBUG')){
+            //httpを強制的にhttpsにする
+            $imageURL = str_replace("http://","https://",$imageURL);
+        }
+        $action = new UriTemplateActionBuilder("HPの問い合わせフォームはこちら","https://manabiail-steam.com/contact");
+        $ccs[] = new CarouselColumnTemplateBuilder("お問い合わせ下さい","左記で解決できない場合は\n恐れ入りますが、サポーターに直接お問い合わせ下さい",$imageURL,[$action],null,null);
+
+        //MessageBuilderをinterfaceとして持つ、TemplateMessageBuilderを返す
+        $multi->add(new TemplateMessageBuilder("新規登録案内",new CarouselTemplateBuilder($ccs,null,null),null,null));
+
+        return $multi;
+    }
+}
--- app/Http/Controllers/LINEAPIController.php
+++ app/Http/Controllers/LINEAPIController.php
@@ -4,16 +4,21 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Log;
-use LINE\LINEBot\HTTPClient\CurlHTTPClient;
-use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 use LINE\LINEBot;
+use LINE\LINEBot\Response;
+use LINE\LINEBot\HTTPClient\CurlHTTPClient;
+
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
 
 use App\Models\Student;
 use App\Models\LineUser;
 use App\Models\EntexHistory;
 
+use App\Common\LINEMessageBuilder;
+
 class LINEAPIController extends Controller
 {
+    /* ref Sticker List : https://developers.line.biz/ja/docs/messaging-api/sticker-list/#sticker-definitions */
     //
     //LINEのpushメッセージを送ります
     public static function linePushMessage(string $userId,string $message){
@@ -42,178 +47,188 @@ class LINEAPIController extends Controller
         $httpClient = new CurlHTTPClient(env('LINE_CHANEL_A_TOKEN'));
         //チャネルシークレットをセット
         $bot = new LINEBot($httpClient, ['channelSecret' => env('LINE_CHANEL_SECRET')]);
+       
         
         $request->collect('events')->each(function($event) use($bot){
 
+            //LINEMessageオブジェクト
+            $l_message = new LINEMessageBuilder();
+
+            //共通プロパティの取得
             $eventType = $event['type'];
-            $messageType = $event['message']['type'];
-            $replyToken = $event['replyToken'];
 
+            //イベントタイプ別に処理を分岐する
+            if($eventType=='postback'){
 
-            //イベントがメッセージ以外だったら終了
-            if($eventType != 'message'){
+                $replyToken = $event['replyToken'];
+                $data = $event['postback']['data'];
+                Log::info('postbackイベントのデータです',[$data,$replyToken]);
+
+                //ポストバックの内容によって処理を分岐する
+                if($data == LINEMessageBuilder::PB_INDI_NEWREGISTRATION){
+
+                    //返答メッセージを送る
+                    $res= $bot->replyMessage($replyToken,$l_message->introduceRegistration());
+                    
+                    Log::info('postbackへの返答結果',[$res,$res->getRawBody()]);
+
+                }
+                return '';
+            }
+            else if($eventType=='message'){
+                /*messageタイプの場合の処理*/
+                $replyToken = $event['replyToken'];
+                $messageType = $event['message']['type'];
+                
+                //メッセージが文字以外だったら終了
+                if($messageType !='text'){
+                    $bot->replyMessage($replyToken,$l_message->nonApplicable());
+                    return '';
+                }
+
+                //テキストを取得(message-typeがtextじゃないと、セットされておらずエラーになるので、このタイミング)
+                $messageText = $event['message']['text'];
+
+                /* 1. 初期登録用 */
+                // 山田太郎-1234 
+                // -で分割して、右4桁が数字がどうかで判定します
+                $splited = explode('-',$messageText);
+                if(count($splited)==2 and is_numeric($splited[1])){
+                    Log::Info('lineWebhookの初期登録処理が開始されました',[$messageText]);
+
+                    //名称、識別コードとStudentマスタの突合を行う
+                    $student = Student::where('verificationName',$splited[0])->where('verificationCode',$splited[1])->first();
+                    
+                    /* Studentと突合出来なかった場合 */
+                    if(empty($student)){
+                        Log::Info('初期登録処理でStudentマスタと一致しませんでした',[$messageText]);
+
+                        $bot->replyMessage($replyToken,$l_message->notMatch());
+
+                    }
+                    /* Studentと突合できた場合 */
+                    else{
+                        Log::Info('初期登録処理でStudentマスタと一致しました',[$messageText]);
+
+                        //UserIdの取得
+                        $userId = $event['source']['userId'];
+                        
+                        //すでに登録されているかのチェック
+                        $lineuser = LineUser::where('lineUserId',$userId)->where('student_id',$student->id)->first();
+                        
+                        /* すでに登録されている場合 */
+                        if(!empty($lineuser)){
+                            Log::Info('すでに登録されていました',[$messageText]);
+
+                            $bot->replyMessage($replyToken,$l_message->alreadyExists());
+        
+                        }
+                        /* 登録がまだの場合 */
+                        else{
+                            Log::Info('ユーザ登録処理を開始します',[$messageText]);
+                            //LINEプロファイル情報の取得
+                            $profileRes = $bot->getProfile($userId);
+                            $displayName='';
+                            if($profileRes->isSucceeded()){
+
+                                $profile = $profileRes->getJSONDecodedBody();
+                                $displayName = $profile['displayName'];
+
+                                //ユーザー登録処理
+                                $lineUser = new LineUser();
+                                $lineUser->student_id = $student->id;
+                                $lineUser->lineDisplayName = $displayName;
+                                $lineUser->lineUserId = $userId;
+
+                                $lineUser->save();
+
+                                Log::Info('ユーザ登録処理が完了',[$lineUser]);
+
+                                //登録完了メッセージを送る
+                                $bot->replyMessage($replyToken,$l_message->registerCompleted());
+                            }
+                            else{
+                                //ここにくる想定はないがなんらかの理由によりプロファイル情報が取得できなかった場合
+                                Log::Info('LINEプロファイルが取得できませんでした',[$messageText,$lineUser]);
+
+                                $bot->replyMessage($replyToken,$l_message->registerCanceled());
+
+                            }
+
+                        }
+                    }
+
+                } /* 1. 初期登録用 ここまで *
+
+                /* 2.入退室履歴確認 */
+                //「入退室履歴を確認する」というメッセージが届いたら
+                else if($messageText == env('LINE_BROWSEENTEX')){
+                    //返信メッセージ
+                    $resMessages = [];
+
+                    //プロファイルからテーブルを参照し、対象の生徒を取得する
+                    $lineUserId = $event['source']['userId'];
+                    $users = LINEUser::where('lineUserId',$lineUserId)->get();
+
+                    if(count($users)<1){
+                    
+                        //まだ登録が済んでいない人が送ってきた場合
+                        $resMessages[]='初期登録がまだのようです';
+                        $bot->replyMessage($replyToken,$l_message->notRegistered());
+
+                        return;
+                    }
+                    else{
+                        $bot->replyMessage($replyToken,$l_message->entexHistory($users));  
+                        return;
+                    }
+                    // //入退室履歴確認時のメッセージをまとめて送ります（応答は1回しかできないため）
+                    // //TextMessageBuilderに配列を渡すと上手くいかないので
+                    // switch(count($resMessages)){
+                    //     case 1:
+                    //         $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0]));
+                    //         break;
+                    //     case 2:
+                    //         $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0],$resMessages[1]));
+                    //         break;
+                    //     case 3:
+                    //         $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0],$resMessages[1],$resMessages[2]));
+                    //         break;
+                    //     case 4:
+                    //         $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0],$resMessages[1],$resMessages[2],$resMessages[3]));
+                    //         break;
+                    //     default:
+                    //         break;
+                    //     //Warning 4人兄弟姉妹までしか想定しない
+                    // }
+                    
+                    // 検証履歴
+                    // $bot->replyMessage($replyToken,new TextMessageBuilder('ここまできた')); //success
+                    // $bot->replyMessage($replyToken,new TextMessageBuilder("りんご",["ぶどう","みかん"]));//NG 200
+                    // $textMessageBuilder = new TextMessageBuilder(array("ぶどう","みかん")); //NG
+                    // $bot->replyMessage($replyToken,new TextMessageBuilder("ぶどう","みかん")); //success
+                    // $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0],$resMessages[1])); //success
+
+                }/* 2.入退室履歴確認 ここまで*/
+                else{
+                    // $bot->replyText($replyToken,"本チャットでお答えできる内容ではないようです。\n恐れ入りますが、サポーターにお問い合わせください。");
+                    // $bot->replyMessage($replyToken,new TextMessageBuilder("本チャットでお答えできる内容ではないようです。\n恐れ入りますが、サポーターにお問い合わせください。"));
+                    $bot->replyMessage($replyToken,$l_message->nonApplicable());
+                }
+            }
+            //イベントがpostback メッセージ以外だったら終了
+            else{
+                Log::Info('message以外のイベントでした',[$eventType]);
                 //メッセージイベント以外は何もしない
                 return '';
             }
-            //メッセージが文字以外だったら終了
-            if($messageType !='text'){
-                $bot->replyText($replyToken,"本チャットでお答えできる内容ではないようです。\n恐れ入りますが、サポーターにお問い合わせください。");
-                return '';
-            }
 
-            //テキストを取得(message-typeがtextじゃないと、セットされておらずエラーになるので、このタイミング)
-            $messageText = $event['message']['text'];
 
-            /* 1. 初期登録用 */
-            // 山田太郎-1234 
-            // -で分割して、右4桁が数字がどうかで判定します
-            $splited = explode('-',$messageText);
-            if(count($splited)==2 and is_numeric($splited[1])){
-                Log::Info('lineWebhookの初期登録処理が開始されました',[$messageText]);
-
-                //メッセージとStudentマスタの突合を行う
-                $student = Student::where('verificationName',$splited[0])->where('verificationCode',$splited[1])->first();
-                
-                if(empty($student)){
-                    /* Studentと突合出来なかった場合 */
-                    Log::Info('初期登録処理でStudentマスタと一致しませんでした',[$messageText]);
-
-                    $bot->replyMessage($replyToken,new TextMessageBuilder("お子様の名前か識別コードが異なるようです。\nお子様の名前に識別コードを添えて送信してください。\n例)山田太郎-5671"));
-
-                }
-                else{
-                    /* Studentと突合できた場合 */
-                    Log::Info('初期登録処理でStudentマスタと一致しました',[$messageText]);
-
-                    //UserIdの取得
-                    $userId = $event['source']['userId'];
-                    
-                    //すでに登録されているかのチェック
-                    $lineuser = LineUser::where('lineUserId',$userId)->where('student_id',$student->id)->first();
-                    
-                    if(!empty($lineuser)){
-                        /* すでに登録されている場合 */
-                        Log::Info('すでに登録されていました',[$messageText]);
-
-                        $bot->replyMessage($replyToken,new TextMessageBuilder("既にご登録頂いています"));
-    
-                    }
-                    else{
-                        /* 登録がまだの場合 */
-                        Log::Info('ユーザ登録処理を開始します',[$messageText]);
-                        //LINEプロファイル情報の取得
-                        $profileRes = $bot->getProfile($userId);
-                        $displayName='';
-                        if($profileRes->isSucceeded()){
-                            $profile = $profileRes->getJSONDecodedBody();
-                            $displayName = $profile['displayName'];
-                        }
-
-                        //ユーザー登録処理
-                        $lineUser = new LineUser();
-                        $lineUser->student_id = $student->id;
-                        $lineUser->lineDisplayName = $displayName;
-                        $lineUser->lineUserId = $userId;
-
-                        $lineUser->save();
-
-                        $bot->replyMessage($replyToken,new TextMessageBuilder("正しく登録できました。ありがとうございました。"));
-
-                        Log::Info('ユーザ登録処理が完了',[$messageText]);
-
-                    }
-                }
-
-            } /* 1. 初期登録用 ここまで *
-
-            /* 2.入退室履歴確認 */
-            //「入退室履歴を確認する」というメッセージが届いたら
-            if($messageText == env('LINE_BROWSEENTEX')){
-                //返信メッセージ
-                $resMessages = [];
-
-                //プロファイルからテーブルを参照し、対象の生徒を取得する
-                $lineUserId = $event['source']['userId'];
-                $users = LINEUser::where('lineUserId',$lineUserId)->get();
-
-                if(count($users)<1){
-                
-                    //まだ登録が済んでいない人が送ってきた場合
-                    // $bot->replyText($replyToken,'初期登録がまだのようです');
-                    $resMessages[]='初期登録がまだのようです';
-                }
-                else{
-                    
-                    foreach($users as $user){
-                        //生徒単位のループ
-
-                        //入退室履歴テーブルから情報を取得する
-                        $litems = EntexHistory::getEntexHistory($user->student_id);
-                        $student = Student::find($user->student_id);
-
-                        $message = "";
-                        if(count($litems)<= 0){
-                            $message="まだ入退室履歴がありません";
-                            // $bot->replyMessage($replyToken,new TextMessageBuilder($message));success
-                        }
-                        else{
-
-                            // $message = $student->messageDispName."さんの入退室履歴";//NG
-                            $message = ($student->messageDispName)."さんの入退室履歴\n\n";//( )がないとエラー
-
-                            $i = 0;
-                            foreach($litems as $item){
-                                if($i>9){
-                                    break;
-                                }
-                                $typeName = $item->type=='1'?'入室':'退室';
-                                $message = $message.date('m/d H:i', strtotime(($item->entex_datetime)))." ".$typeName."\n";
-                                
-                                $i++;
-                            }
-                            $message=$message."※直近10件を表示しています";
-                        }
-                        //生徒毎に1メッセージ
-                        // $bot->replyMessage($replyToken,new TextMessageBuilder($message));
-
-                        $resMessages[]=$message;
-                    }
-                }
-                //入退室履歴確認時のメッセージをまとめて送ります（応答は1回しかできないため）
-                //TextMessageBuilderに配列を渡すと上手くいかないので
-                switch(count($resMessages)){
-                    case 1:
-                        $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0]));
-                        break;
-                    case 2:
-                        $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0],$resMessages[1]));
-                        break;
-                    case 3:
-                        $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0],$resMessages[1],$resMessages[2]));
-                        break;
-                    case 4:
-                        $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0],$resMessages[1],$resMessages[2],$resMessages[3]));
-                        break;
-                    default:
-                        break;
-                    //Warning 4人兄弟姉妹までしか想定しない
-                }
-                
-                // 検証履歴
-                // $bot->replyMessage($replyToken,new TextMessageBuilder('ここまできた')); //success
-                // $bot->replyMessage($replyToken,new TextMessageBuilder("りんご",["ぶどう","みかん"]));//NG 200
-                // $textMessageBuilder = new TextMessageBuilder(array("ぶどう","みかん")); //NG
-                // $bot->replyMessage($replyToken,new TextMessageBuilder("ぶどう","みかん")); //success
-                // $bot->replyMessage($replyToken,new TextMessageBuilder($resMessages[0],$resMessages[1])); //success
-
-            }/* 2.入退室履歴確認 ここまで*/
-            else{
-                // $bot->replyText($replyToken,"本チャットでお答えできる内容ではないようです。\n恐れ入りますが、サポーターにお問い合わせください。");
-                $bot->replyMessage($replyToken,new TextMessageBuilder("本チャットでお答えできる内容ではないようです。\n恐れ入りますが、サポーターにお問い合わせください。"));
-            }
         });
 
         return ;
     }
+    
+
    
 }
