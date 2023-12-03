04907f77c0ca9916782baa000b169ad60e4392bf
hash-home-hacmo <hacmotshiro2@gmail.com>
2023-12-03 12:32:48 +0900

    405エラー画面追加

---
+++ resources/views/errors/405.blade.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>ERROR</title>
+        
+        <!-- Scripts -->
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+        @vite(['resources/css/app.css', 'resources/js/app.js'])
+    </head>
+<body>
+    <div class="bg-white py-6 sm:py-8 lg:py-12">
+    <div class="max-w-screen-2xl px-4 md:px-8 mx-auto">
+        <div class="flex flex-col items-center">
+
+        <p class="text-indigo-500 text-sm md:text-base font-semibold uppercase mb-4">405</p>
+        <h1 class="text-gray-800 text-2xl md:text-3xl font-bold text-center mb-2">予期せぬリクエストがされました</h1>
+        <p class="max-w-screen-md text-gray-500 md:text-lg text-center mb-12">トップページに戻ってください</p>
+        <a href="/entex/lrs" class="inline-block bg-indigo-500 hover:bg-indigo-600 focus-visible:ring ring-indigo-400 text-white text-xl md:text-2xl font-semibold text-center rounded-md outline-none transition duration-100 px-10 py-6 mb-8">入退室処理トップ</a>
+        <a href="/" class="inline-block bg-gray-200 hover:bg-gray-300 focus-visible:ring ring-indigo-300 text-gray-500 active:text-gray-700 text-sm md:text-base font-semibold text-center rounded-md outline-none transition duration-100 px-8 py-3">トップページ</a>
+        </div>
+    </div>
+    </div>
+</body>
+</html>
